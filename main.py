ac89d673822a41873db8526c631acf134f2e4963
Ari Porad <ari@ariporad.com>
2016-03-17 16:05:36 -0700

    fix: Properly reset terminal settings even if an error is thrown

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ def main():
     stdout_attrs = termios.tcgetattr(1)
     stderr_attrs = termios.tcgetattr(2)
 
-    reader = MapReader()
-    gamemap, pacman, ghosts = reader.read('gamemap.txt')
-    g = Game(gamemap, pacman, ghosts)
-    g.start()
-
-    # If we get here, then we need to clean up
-    termios.tcsetattr(0, termios.TCSANOW, stdin_attrs)
-    termios.tcsetattr(1, termios.TCSANOW, stdout_attrs)
-    termios.tcsetattr(2, termios.TCSANOW, stderr_attrs)
+    try:
+        reader = MapReader()
+        gamemap, pacman, ghosts = reader.read('gamemap.txt')
+        g = Game(gamemap, pacman, ghosts)
+        g.start()
+    finally:
+        termios.tcsetattr(0, termios.TCSANOW, stdin_attrs)
+        termios.tcsetattr(1, termios.TCSANOW, stdout_attrs)
+        termios.tcsetattr(2, termios.TCSANOW, stderr_attrs)
 
 
 if __name__ == '__main__':
